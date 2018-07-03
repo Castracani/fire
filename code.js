8c9554d6138184271eb14b59a866e1e0c30d25b4
Castracani <willmyers993@gmail.com>
2018-07-03 19:12:23 -0400

    resoundinggit add -A!

--- code.js
+++ code.js
@@ -32,44 +32,16 @@ $(document).ready(function () {
     }
   };
 
-$("#submit-btn").click(function (user) {
-  var email = $("#email").val().trim();
-  var username = $("#username").val().trim();
-  var password = $("#password").val().trim();
-  $("#email").val("");
-  $("#username").val("");
-  $("#password").val("");
-  if (user != null) {
-    firebase.auth().signOut();
-  }
-  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-    .then(function () {
-      // Existing and future Auth states are now persisted in the current
-      // session only. Closing the window would clear any existing state even
-      // if a user forgets to sign out.
-      // ...
-      // New sign-in will be persisted with session persistence.
-      return firebase.auth().createUserWithEmailAndPassword(email, password);
-    })
-    .catch(function (error) {
-      // Handle Errors here.
-      var errorMessage = error.message;
-      window.alert("Error: " + errorMessage);
-    });
-});
-
-$("#login-btn").click(function (user) {
-  var user = firebase.auth().currentUser;
-  var email = $("#email").val().trim();
-  var username = $("#username").val().trim();
-  var password = $("#password").val().trim();
-  $("#email").val("");
-  $("#username").val("");
-  $("#password").val("");
-  if (user != null) {
-    console.log("Hi!");
-    window.alert("You are already signed into an account. Please sign out before attempting to log into a different account.");
-  } else {
+  $("#submit-btn").click(function (user) {
+    var email = $("#email").val().trim();
+    var username = $("#username").val().trim();
+    var password = $("#password").val().trim();
+    $("#email").val("");
+    $("#username").val("");
+    $("#password").val("");
+    if (user != null) {
+      firebase.auth().signOut();
+    }
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
       .then(function () {
         // Existing and future Auth states are now persisted in the current
@@ -77,19 +49,47 @@ $("#login-btn").click(function (user) {
         // if a user forgets to sign out.
         // ...
         // New sign-in will be persisted with session persistence.
-        return firebase.auth().signInWithEmailAndPassword(email, password);
+        return firebase.auth().createUserWithEmailAndPassword(email, password);
       })
       .catch(function (error) {
         // Handle Errors here.
         var errorMessage = error.message;
         window.alert("Error: " + errorMessage);
       });
-  }
-});
+  });
 
-$("#signout-btn").click(function () {
-  firebase.auth().signOut();
-});
+  $("#login-btn").click(function (user) {
+    var user = firebase.auth().currentUser;
+    var email = $("#email").val().trim();
+    var username = $("#username").val().trim();
+    var password = $("#password").val().trim();
+    $("#email").val("");
+    $("#username").val("");
+    $("#password").val("");
+    if (user != null) {
+      console.log("Hi!");
+      window.alert("You are already signed into an account. Please sign out before attempting to log into a different account.");
+    } else {
+      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function () {
+          // Existing and future Auth states are now persisted in the current
+          // session only. Closing the window would clear any existing state even
+          // if a user forgets to sign out.
+          // ...
+          // New sign-in will be persisted with session persistence.
+          return firebase.auth().signInWithEmailAndPassword(email, password);
+        })
+        .catch(function (error) {
+          // Handle Errors here.
+          var errorMessage = error.message;
+          window.alert("Error: " + errorMessage);
+        });
+    }
+  });
+
+  $("#signout-btn").click(function () {
+    firebase.auth().signOut();
+  });
 
 
   //very last brackets (Document Ready) below here!  
