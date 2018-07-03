a87dc89b126fe603346f7f175076065501b459c9
Castracani <willmyers993@gmail.com>
2018-07-03 19:10:56 -0400

    resounding

--- code.js
+++ code.js
@@ -5,25 +5,24 @@ $(document).ready(function () {
   var user = firebase.auth().currentUser;
 
   //firebase.auth().onAuthStateChanged(function (user) {
-    //var user = firebase.auth().currentUser;
-    //var name, email, photoUrl, uid, emailVerified;
+  //var user = firebase.auth().currentUser;
+  //var name, email, photoUrl, uid, emailVerified;
 
 
-    if (user) {
-      // User is signed in.
-      console.log("There's a user signed in!");
-      $("#logged-in").css("display", "inline-block");
-      $("#not-logged-in").css("display", "none");
+  if (user) {
+    // User is signed in.
+    console.log("There's a user signed in!");
+    $("#logged-in").css("display", "inline-block");
+    $("#not-logged-in").css("display", "none");
 
-      if (user != null) {
-        var emailID = user.email;
-        $("#welcome-text").text(" Happy to have you here, " + emailID + "!");
-        name = user.displayName;
-        email = user.email;
-        emailVerified = user.emailVerified;
-        uid = user.uid;
-        console.log(name, email, emailVerified, uid);                           
-      }
+    if (user != null) {
+      var emailID = user.email;
+      $("#welcome-text").text(" Happy to have you here, " + emailID + "!");
+      name = user.displayName;
+      email = user.email;
+      emailVerified = user.emailVerified;
+      uid = user.uid;
+      console.log(name, email, emailVerified, uid);
     } else {
       // No user is signed in.
       console.log("Nobody's here!");
@@ -31,18 +30,46 @@ $(document).ready(function () {
       $("#not-logged-in").css("display", "inline-block");
       $("#welcome-text").empty();
     }
-  });
+  };
 
-  $("#submit-btn").click(function (user) {
-    var email = $("#email").val().trim();
-    var username = $("#username").val().trim();
-    var password = $("#password").val().trim();
-    $("#email").val("");
-    $("#username").val("");
-    $("#password").val("");
-    if (user != null) {
-      firebase.auth().signOut();
-    }
+$("#submit-btn").click(function (user) {
+  var email = $("#email").val().trim();
+  var username = $("#username").val().trim();
+  var password = $("#password").val().trim();
+  $("#email").val("");
+  $("#username").val("");
+  $("#password").val("");
+  if (user != null) {
+    firebase.auth().signOut();
+  }
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function () {
+      // Existing and future Auth states are now persisted in the current
+      // session only. Closing the window would clear any existing state even
+      // if a user forgets to sign out.
+      // ...
+      // New sign-in will be persisted with session persistence.
+      return firebase.auth().createUserWithEmailAndPassword(email, password);
+    })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+    });
+});
+
+$("#login-btn").click(function (user) {
+  var user = firebase.auth().currentUser;
+  var email = $("#email").val().trim();
+  var username = $("#username").val().trim();
+  var password = $("#password").val().trim();
+  $("#email").val("");
+  $("#username").val("");
+  $("#password").val("");
+  if (user != null) {
+    console.log("Hi!");
+    window.alert("You are already signed into an account. Please sign out before attempting to log into a different account.");
+  } else {
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
       .then(function () {
         // Existing and future Auth states are now persisted in the current
@@ -50,47 +77,19 @@ $(document).ready(function () {
         // if a user forgets to sign out.
         // ...
         // New sign-in will be persisted with session persistence.
-        return firebase.auth().createUserWithEmailAndPassword(email, password);
+        return firebase.auth().signInWithEmailAndPassword(email, password);
       })
       .catch(function (error) {
         // Handle Errors here.
         var errorMessage = error.message;
         window.alert("Error: " + errorMessage);
       });
-  });
+  }
+});
 
-  $("#login-btn").click(function (user) {
-    var user = firebase.auth().currentUser;
-    var email = $("#email").val().trim();
-    var username = $("#username").val().trim();
-    var password = $("#password").val().trim();
-    $("#email").val("");
-    $("#username").val("");
-    $("#password").val("");
-    if (user != null) {
-      console.log("Hi!");
-      window.alert("You are already signed into an account. Please sign out before attempting to log into a different account.");
-    } else {
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        .then(function () {
-          // Existing and future Auth states are now persisted in the current
-          // session only. Closing the window would clear any existing state even
-          // if a user forgets to sign out.
-          // ...
-          // New sign-in will be persisted with session persistence.
-          return firebase.auth().signInWithEmailAndPassword(email, password);
-        })
-        .catch(function (error) {
-          // Handle Errors here.
-          var errorMessage = error.message;
-          window.alert("Error: " + errorMessage);
-        });
-    }
-  });
-
-  $("#signout-btn").click(function () {
-    firebase.auth().signOut();
-  })
+$("#signout-btn").click(function () {
+  firebase.auth().signOut();
+});
 
 
   //very last brackets (Document Ready) below here!  
